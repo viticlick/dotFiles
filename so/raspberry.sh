786cbdd77709cb4e5a6d7af131056117d7570fe2
Víctor López <viticlick@users.noreply.github.com>
2021-02-07 23:32:52 +0100

    Update raspberry.sh

--- so/raspberry.sh
+++ so/raspberry.sh
@@ -1,7 +1,7 @@
 #!/usr/env sh
 apt-get update -y 
 apt-get upgrade -y
-apt-get install vim git curl npm fzf
+apt-get install vim git curl npm fzf sqlite3
 apt-get install tldr
 apt-get install tig
 apt-get install nodejs
