97afef2090f43d131566d6a317f6813bdfef239d
Víctor López <developer@victorlopez.eu>
2021-02-14 09:05:43 +0100

    [UBUNTU] Add jq to list of programs to install

--- so/ubuntu/debScript.sh
+++ so/ubuntu/debScript.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/sh
 apt-get update -y 
 apt-get upgrade -y
-apt-get install vim git curl npm fzf
+apt-get install vim git curl npm fzf jq -y
 apt-get install zsh
 apt-get install tldr
 apt-get install tig
