8f13bca8eed89dd26ab596e46cdddb3065d60bdd
Víctor López <developer@victorlopez.eu>
2021-02-20 22:18:13 +0100

    [UBUNTU] Add ripgrep and silversearcher-ag

--- so/ubuntu/debScript.sh
+++ so/ubuntu/debScript.sh
@@ -2,6 +2,7 @@
 apt-get update -y 
 apt-get upgrade -y
 apt-get install vim git curl npm fzf jq -y
+apt-get install ripgrep silversearcher-ag -y
 apt-get install zsh
 apt-get install tldr
 apt-get install tig
