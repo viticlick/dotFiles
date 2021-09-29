5be83ff8ce2a64ae29f364b9faf665e7ca4150f3
Víctor López <viticlick@gmail.com>
2021-09-29 21:42:31 +0100

    Move tmux configs again

--- so/raspberry.sh
+++ so/raspberry.sh
@@ -1,4 +1,4 @@
-#!/usr/env sh
+# !/bin/bash
 apt-get update -y 
 apt-get upgrade -y
 apt-get install vim git curl npm fzf sqlite3 jq -y
@@ -6,5 +6,4 @@ apt-get install tldr
 apt-get install tig
 apt-get install nodejs
 apt-get install tmux
-apt-get install mosquito
 
